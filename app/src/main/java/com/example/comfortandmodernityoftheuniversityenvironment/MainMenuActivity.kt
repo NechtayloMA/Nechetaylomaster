bc0f6a931f251d78f20fdfd7c1718c68c82f6cca
NechtayloMA <omikheev@mail.ru>
2025-10-26 14:23:04 +0300

    Create XML layouts for main screen

--- app/src/main/java/com/example/comfortandmodernityoftheuniversityenvironment/MainMenuActivity.kt
+++ app/src/main/java/com/example/comfortandmodernityoftheuniversityenvironment/MainMenuActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainMenuActivity : AppCompatActivity() {
+//class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
